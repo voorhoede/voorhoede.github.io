083422dd48805e7e9d7ba680e9854c2bc26e1441
Jasper Moelker <j.b.moelker@activeids.nl>
2016-11-15 13:54:07 +0100

    chore(travis): deploy develop to master

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e # exit with nonzero exit code if anything fails
+
+if [[ $TRAVIS_BRANCH == "develop" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+
+echo "Starting to update master\n"
+
+#copy data we're interested in to other place
+cp -R dist $HOME/dist
+
+#go to home and setup git
+cd $HOME
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+#using token clone master branch
+git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git master > /dev/null
+
+#go into directory and copy data we're interested in to that directory
+cd master
+cp -Rf $HOME/dist/* .
+
+echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
+
+#add, commit and push files
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin master > /dev/null
+
+echo "Done updating master\n"
+
+else
+ echo "Skipped updating master, because build is not triggered from the develop branch."
+fi;
